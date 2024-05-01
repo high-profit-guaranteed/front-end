264f561d786c1b5b9a6b2de518781e69db674132
suhyeon01 <gsuhyeon58@gmail.com>
2024-05-02 01:32:51 +0900

    24.05.01

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,92 +1,97 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useEffect, useState } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Topbar from '../components/Topbar.jsx';
+import Chart from 'chart.js/auto';
 
-const style = {
+const styles = {
   container: {
-    marginLeft: '200px',
+    display: 'grid',
+    marginLeft: '250px',
     marginRight: '30%',
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "20px",
-    padding: "30px",
-    borderRadius: "50px",
-    background: "#f2f6ef",
-    boxShadow: "20px 20px 24px 10px rgba(0,0,0,0.1)",
+    marginTop: '80px',
+    marginBottom: '20px',
+    padding: '20px',
+    backgroundColor: '#f2f6ef',
+    borderRadius: '10px',
   },
   section: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "relative",
-    overflow: "hidden",
+    marginBottom: '20px',
   },
-  column: {
-    display: "flex",
-    width: "100%",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    flexGrow: 1,
-    position: "relative",
-    overflow: "hidden",
-    paddingLeft: "30px",
-    paddingRight: "30px",
+  sectionTitle: {
+    fontSize: '20px',
+    marginBottom: '10px',
   },
-  heading: {
-    cursor: 'pointer',
-    width: "100%",
-    fontSize: "1.3vw",
-    textAlign: "center",
-    color: "#000",
-  },
-  bar: {
-    alignSelf: "stretch",
-    width: "100%",
-    height: "5px",
-    borderRadius: "10px",
-    background: "#5a5a5a",
-  },
-  whiteBox: {
-    gridColumn: "1 / -1",
-    width: "100%",
-    height: "300px",
-    background: "#fff",
-    borderRadius: "10px",
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-    marginTop: "5px",
-  },
+  sectionContent: {
+    marginLeft: '0%',
+    marginRight: '0.1%',
+    backgroundColor: '#fff',
+    borderRadius: '10px',
+    padding: '25px',
+  }
 };
 
-
 function Home() {
+  const chartRef = useRef(null);
+  const [showGraph, setShowGraph] = useState(true);
+
+  // 그래프는 수정
+  useEffect(() => {
+    if (showGraph) {
+      const ctx = chartRef.current.getContext('2d');
+      const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+            label: '총 판매 주식',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {}
+      });
+      return () => {
+        myChart.destroy();
+      };
+    }
+  }, [showGraph]);
+
+  const handleButtonClick = () => {
+    setShowGraph(!showGraph);
+  };
+
   return (
     <div>
       <Navbar />
       <Topbar />
-      <br/><br/>
-
-      <div style={style.container}>
-        <div style={style.section}>
-          <div style={style.column}>
-            <p style={style.heading}>총 자산 평균</p>
-            <div style={style.bar} />
-          </div>
-          <div style={style.column}>
-            <p style={style.heading}>주문 내역</p>
-            <div style={style.bar} />
-          </div>
-          <div style={style.column}>
-            <p style={style.heading}>대기 주문</p>
-            <div style={style.bar} />
+      <div style={styles.container}>
+        <div style={styles.section}>
+          <h2 style={styles.sectionTitle}>자산 현황</h2>
+          <div style={styles.sectionContent}>
+            <canvas ref={chartRef}></canvas>
           </div>
         </div>
-        <div style={style.whiteBox}></div>
+      </div>
+      <div style={styles.container}>
+      <div style={styles.section}>
+          <h2 style={styles.sectionTitle}>매출액</h2>
+          <div style={styles.sectionContent}>
+            <p>매출액 내용</p>
+          </div>
+        </div>
+      </div>
+      <div style={styles.container}>
+        <div style={styles.section}>
+          <h2 style={styles.sectionTitle}>대기 주문</h2>
+          <div style={styles.sectionContent}>
+            <p>대기 주문 내용</p>
+          </div>
+        </div>
       </div>
     </div>
+    
   );
 }
 
--- front/src/pages/News.jsx
+++ front/src/pages/News.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from '../components/Navbar.jsx'
 import Topbar from '../components/Topbar.jsx';
 
+const styles = {
+};
+
 function News() {
   return (
     <div>
